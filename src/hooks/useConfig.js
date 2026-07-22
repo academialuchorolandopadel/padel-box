3ecf3954a9da832914b1200b01f0b6cc77fd70ff
academialuchorolandopadel <abcconstruccionesenseco@gmail.com>
2026-07-22 09:11:14 -0300

    Add parrillaPrecio to DEFAULT configuration

--- src/hooks/useConfig.js
+++ src/hooks/useConfig.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../lib/firebase";
 
-const DEFAULT = { valorHora: { box1: 80000, box2: 100000, box3: 100000 }, tuboPrecio: 80000 };
+const DEFAULT = { valorHora: { box1: 80000, box2: 100000, box3: 100000 }, tuboPrecio: 80000, parrillaPrecio: 50000 };
 
 export function useConfig() {
   const [config, setConfig] = useState(DEFAULT);
